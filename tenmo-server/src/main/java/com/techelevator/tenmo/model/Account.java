b16e5aff40387727841122f4ec38fc82c8c3eaae
Staci <stacipugh@gmail.com>
2023-11-29 11:17:29 -0500

    Added Account and History Models

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,46 @@
+package com.techelevator.tenmo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+public class Account {
+    @JsonProperty("account_id")
+    private int accountId;
+    @JsonProperty("user_id")
+    private int userId;
+    private BigDecimal balance;
+
+    public Account(int accountId, int userId, double balance){
+        this.accountId = accountId;
+        this.userId = userId;
+        this.balance = BigDecimal.valueOf(balance);
+    }
+    public int getAccountId(){
+        return accountId;
+    }
+    public int getUserId(){
+        return userId;
+    }
+    public BigDecimal getBalance(){
+        return balance;
+    }
+    public void setAccountId(int accountId){
+        this.accountId = accountId;
+    }
+    public void setUserId(int userId){
+        this.userId = userId;
+    }
+    public void setBalance(BigDecimal balance){
+        this.balance = balance;
+    }
+
+    //these may not be necessary
+        //created just in case
+    public double getBalanceDouble(){
+        return balance.doubleValue();
+    }
+    public void setBalanceDouble(double balance){
+        this.balance = BigDecimal.valueOf(balance);
+    }
+}
